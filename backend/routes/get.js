fec01b39cdf5185c8696b1a5b80dbe5be272124d
huka <4g5gr55y55y@gmail.com>
2024-07-01 23:44:26 +0300

    change path for main layout in get.js

--- backend/routes/get.js
+++ backend/routes/get.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname + '/../client/' });
+    res.sendFile('index.html');
 });
 
 module.exports = router;
